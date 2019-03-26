cf246b46f88574a01c9d9c37fb5c9b01851a914c
leonelnieto <lnieto@utah.gov>
2019-03-26 16:36:28 -0600

    Course modal

--- js/plugins/emp-dev-calendar.js
+++ js/plugins/emp-dev-calendar.js
@@ -5,14 +5,30 @@ fetch(
     return response.json();
   })
   .then(function(j) {
-    console.log(j);
+    console.log(j[1]["description"]);
     var calendarEl = document.getElementById("calendar");
     var calendar = new FullCalendar.Calendar(calendarEl, {
       header: { center: "dayGridMonth,timeGridWeek" },
       plugins: ["dayGrid", "timeGrid", "list"],
       timeZone: "UTC",
       classNames:["fc-event"],
-      events: j
+      events: j,
+      eventClick: function(info) {
+        var eventObj = info.event;
+          //alert('Clicked ' + eventObj.title);
+          for(var i = 0;i<j.length;i++){
+            if(j[i]["id"] === eventObj.id){
+              $("#desc").html(j[i]["description"])
+              var d = new Date(j[i]["start"])
+              $("#start").html(d);
+              d = new Date(j[i]["end"]);
+              $("#end").html(d);
+            }
+            continue;
+          }
+          $('#exampleModal').modal('show')
+          $("#modalLabel").html(eventObj.title)
+      }
     });
 
     calendar.render();

9486fd2e4aaa72b6df0e437f64805936c73e5526
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-03-25 20:36:13 -0600

    update to json

--- js/plugins/emp-dev-calendar.js
+++ js/plugins/emp-dev-calendar.js
@@ -1,29 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var calendarEl = document.getElementById("calendar");
-  fetch("../../Data/EmpDev/course_schedule_data.json")
-    .then(function(response) {
-      // Convert to JSON
-      return response.json();
-    })
-    .then(function(j) {
-      // Yay, `j` is a JavaScript object
-      console.log(j);
+fetch(
+  "https://leonelnieto.github.io/UDOTDB/Data/EmpDev/course_schedule_data.json"
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(j) {
+    console.log(j);
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: ["dayGrid"],
+      timeZone: "UTC",
+      events: j
     });
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: ["dayGrid"],
-    timeZone: "UTC",
-    events: [
-      {
-        id: "a",
-        title: "my event",
-        start: "2019-03-25T06:30:00",
-        end: "2019-03-25",
-        allDay: false,
-        url: "#",
-        editable: false
-      }
-    ]
-  });
 
-  calendar.render();
-});
+    calendar.render();
+  });

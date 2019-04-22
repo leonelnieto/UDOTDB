b15a6e2f95155941faf0fe6bba5d6d2b072fdd78
daniella <daniellabirch@gmail.com>
2019-04-22 13:43:17 -0600

    update links
    adding variables for links so they always work no matter where they're hosted

--- js/template.js
+++ js/template.js
@@ -1,3 +1,5 @@
+const linkHeader = window.location.href.slice(0, window.location.href.indexOf("dashboard"));
+
 var sidebarTemplate = 
     `<ul class="sidebar navbar-nav toggled">
         <li class="nav-item active">
@@ -54,7 +56,7 @@ var sidebarTemplate =
 var navbarTemplate = 
     `<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
         <a class="navbar-brand mr-1" href="https://www.udot.utah.gov/main/f?p=100:6:0::::V,T:,1">
-            <img src="/img/UDOT-Logo-PNG.png" class="d-inline-block align-top">
+            <img src="${linkHeader}/dashboard/img/UDOT-Logo-PNG.png" class="d-inline-block align-top">
         </a>
         <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
             <i class="fas fa-bars"></i>
@@ -68,4 +70,3 @@ var navbarTemplate =
 
 $('sidebar').html(sidebarTemplate);
 $('navbar').html(navbarTemplate);
-
